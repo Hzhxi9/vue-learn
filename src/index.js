2e081975bea7f6c1bd2afc2ccd94d237752f4e26
huangzx <hzhixi9@gmail.com>
2021-07-20 19:02:40 +0800

    README

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@
  * Vue 构造函数
  * @param {*} options new Vue(options)时传递的配置对象
  */
-export default function Vue(options) {
-  this._init(options);
-}
+// export default function Vue(options) {
+//   this._init(options);
+// }
+
+var removeElement = function (nums, val) {
+  let idx = nums.indexOf(val);
+  while (idx > -1) {
+    nums.splice(idx, 1);
+    idx = nums.indexOf(val, idx + 1);
+  }
+  return nums;
+};
+console.log(removeElement([3, 2, 2, 3], 3));
